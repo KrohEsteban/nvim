c106970df80880434ef53a4db4c978ef34f0602b
Esteban Kroh <ekroh@nubity.com>
2025-03-09 00:45:36 -0300

    First commit whit NvChad

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 
+-- (method 2, for non lazyloaders) to load all highlights at once
+ for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
+   dofile(vim.g.base46_cache .. v)
+ end
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
